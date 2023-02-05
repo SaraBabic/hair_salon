9f4fcabb29d633ce335c45e5d9b0722e9aca61ca
Sara Babić <sarababic0101@gmail.com>
2023-02-05 18:18:37 +0100

    burger

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,15 +1,20 @@
 let scrollpos = window.scrollY;
 
 const header = document.querySelector(".header");
+const links = document.querySelector("#myLinks");
 const scrollChange = 50;
 
 const add_class_on_scroll = () => {
     header.classList.add("colored-header");
     header.classList.remove("transparent-header");
+    links.classList.add("colored-header");
+    links.classList.remove("transparent-header");
 }
 const remove_class_on_scroll = () => {
     header.classList.add("transparent-header");
     header.classList.remove("colored-header");
+    links.classList.add("transparent-header");
+    links.classList.remove("colored-header");
 }
 
 window.addEventListener('scroll', function() {
